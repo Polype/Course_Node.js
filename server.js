fd0d19ddd686d181691632a5bd32ad9dd650e353
Marta <shelia.amazon@gmail.com>
2018-12-04 15:31:07 +0100

    Add project page

--- server.js
+++ server.js
@@ -53,6 +53,13 @@ app.get('/about', (request, response) => {
     });
 });
 
+app.get('/projects', (request, response) => {
+    response.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        welcomeMessage: 'Hello there!'
+    });
+});
+
 app.get('/bad', (request, response) => {
     response.send({
         error: 'Error message'
